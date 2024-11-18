bc030a911e4fc47e337de7e22bff266ee084a8ce
arfin-web <arfinpriom12@gmail.com>
2024-11-18 17:24:57 +0600

    fixing bug

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,5 +1,5 @@
 export const getBaseUrl = (): string => {
-    return process.env.NEXT_PUBLIC_API_BASE_URL || "";
+    return process.env.NEXT_PUBLIC_API_BASE_URL || "https://ecom-manager-backend.vercel.app/api/v1";
 };
 
 // http://localhost:3030/api/v1
